5c5b45c39bdf9cd4c2b9de45987903ea057d8893
zyxd <zyxd@syncraft.ru>
2018-07-04 01:57:55 +0400

    fix: force websockets for socket.io

--- plugins/socket.js
+++ plugins/socket.js
@@ -1,5 +1,7 @@
 import * as io from 'socket.io-client';
 
 export default function(_, inject) {
-  inject('socket', io(process.env.URL));
+  inject('socket', io(process.env.URL, {
+    transports: ['websocket']
+  }));
 }
